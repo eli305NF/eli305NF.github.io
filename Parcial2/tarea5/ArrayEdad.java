41064ad71b90a7d2e05bc39a7c1f3ce7ae0a58b3
Eliud <eliud_304@outlook.com>
2022-11-14 18:42:51 -0600

    2do parcial

---
+++ Parcial2/tarea5/ArrayEdad.java
@@ -0,0 +1,36 @@
+package Parcial2.tarea5;
+
+public class ArrayEdad {
+
+    public static void main(String arg[]) {
+
+// declare an array
+int[] age = new int[10];
+
+// initialize array
+age[0] = 12;
+age[1] = 4;
+age[2] = 5;
+age[3] = 80;
+age[4] = 18;
+age[5] = 19;
+age[6] = 12;
+age[7] = 116;
+age[8] = 10;
+age[9] = 17;
+age[10] = 20;
+
+   System.out.println("array pos 0: " + age[0]);
+   System.out.println("array pos 1 : " + age[1]);
+   System.out.println("array pos 2 : " + age[2]);
+   System.out.println("array pos 3 : " + age[3]);
+   System.out.println("array pos 4 : " + age[4]);
+   System.out.println("array pos 5 : " + age[5]);
+   System.out.println("array pos 6 : " + age[6]);
+   System.out.println("array pos 7 : " + age[7]);
+   System.out.println("array pos 8 : " + age[8]);
+   System.out.println("array pos 9 : " + age[9]);
+   System.out.println("array pos 10 : " + age[10]);
+    }
+}
+
